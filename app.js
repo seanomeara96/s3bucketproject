5e7b3c872283e4726a461cc74a734de3f15b0c04
Sean <seanom96@gmail.com>
2020-03-06 14:51:18 +0000

    s3 file upload actually works this time

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ dotenv.config()
 const MongoClient = require('mongodb').MongoClient;
 const uri = process.env.CONNECTIONSTRING
 const client = new MongoClient(uri, { useUnifiedTopology: true, connectTimeoutMS:60000 });
-
+const multer = require('multer')
+const multerS3 = require('multer-s3')
 AWS.config.update({
     accessKeyId: process.env.AWSACCESSKEYID,
     secretAccessKey: process.env.AWSSECRETKEY
@@ -17,6 +18,18 @@ let s3 = new AWS.S3()
 app.use(express.static('views'))
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
+let upload = multer({
+    storage:multerS3({
+        s3:s3,
+        bucket:process.env.BUCKET,
+        contentType: multerS3.AUTO_CONTENT_TYPE,
+        key: (req, file, cb)=>{
+            cb(null, Date.now().toString()+file.originalname)
+          },
+          acl: 'public-read'
+    })
+})
+//'IMG_20190509_215112_053.jpg'
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname+'/views/index.html'))
 })
@@ -26,34 +39,13 @@ app.get('/thanks',(req,res)=>{
 app.get('/sorry',(req,res)=>{
     res.sendFile(path.join(__dirname+'/views/sorry.html'))
 })
-app.post('/store',(req,res)=>{
-    let params = {
-        Bucket: process.env.BUCKET,
-        Body: req.body.picture,
-        Key: req.body.picture
-    }
-    s3.upload(params,async (err,data)=>{
-        if(err){
-            console.log(err)
-            res.redirect('/sorry')
-        }
-        if(data){
-            console.log('img can be found at', data.Location)
-            let toStore = {
-                "imageLocation":data.Location
-            }
-            await client.connect().then(async ()=>{
-                let collection = await client.db("Images").collection("Images")
-                collection.insertOne(toStore).then((i)=>{
-                    console.log("stored in mongodb at imageLocation: ",i.ops[0].imageLocation)
-                    client.close()
-                })
-              }).catch(err => console.log(err))
-              
-            res.redirect('/thanks')
-        }
-    })
+
+
+app.post('/store',upload.single('picture'),(req,res)=>{
+    console.log(req.file)
+    res.send(`<img src="${req.file.location}" />`)
 })
+
 
 app.listen(port,()=>{
     console.log('app is running on ', port)
